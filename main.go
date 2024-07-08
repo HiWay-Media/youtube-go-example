b28c2fdb1732a1b00eb7c7a60d111539ff4b914d
allan <allan.nava@ies-italia.it>
2024-07-08 11:25:19 +0200

    added demo video

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"google.golang.org/api/youtube/v3"
+)
+
+var (
+	filename = flag.String("filename", "BigBuckBunny_320x180.mp4", "Name of video file to upload")
 )
 
 func main() {
 
 	//appConfig := env.GetEnvConfig()
+
+	flag.Parse()
+
+	if *filename == "" {
+		log.Fatalf("You must provide a filename of a video file to upload")
+	}
 
 	client := getClient(youtube.YoutubeUploadScope)
 
@@ -34,4 +47,16 @@ func main() {
 	log.Println("Uploading video...", service)
 	call := service.Videos.Insert([]string{"snippet", "status"}, upload)
 	log.Println("Uploading video...", call)
+
+	file, err := os.Open(*filename)
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("Error opening %v: %v", *filename, err)
+	}
+
+	response, err := call.Media(file).Do()
+	if err != nil {
+		log.Fatalf("Error making YouTube API call: %v", err)
+	}
+	fmt.Printf("Upload successful! Video ID: %v\n", response.Id)
 }
